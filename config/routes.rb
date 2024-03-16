941e7231a9884b8625e2cc0791bf06fa88d874f7
Doris Xu <dorisxjt@gmail.com>
2024-03-16 15:48:00 +1100

    codes updated without style

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :articles
+
+  # get "articles", to: "articles#index"
+  # get "articles/new", to: "articles#new", as: :new_article
+  # post "articles", to: "articles#create", as: :create_article
+  # get "articles/:id", to: "articles#show", as: :article
+  # get "articles/:id/edit", to: "articles#edit", as: :edit_article
+  # patch "articles/:id", to: "articles#update", as: :update_article
+  # delete "articles/:id", to: "articles#destroy"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'faker'
+
+# Clear existing articles
+Article.destroy_all
+
+# Create 10 fake articles
+10.times do
+  Article.create(
+    title: Faker::Book.title,
+    content: Faker::Lorem.paragraphs(number: 3).join("\n")
+  )
+end
+
+puts "Seed completed!"
